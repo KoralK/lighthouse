14ddf51539ab1112184808c0e43fb6fc33ce991f
Koral <koral@Korals-MacBook-Air.local>
2023-12-10 18:07:42 +0300

    requirements.txt

--- app2.py
+++ app2.py
@@ -1,13 +1,17 @@
 import json
 import requests
 import logging
+import os
+
+# Ensure the NEW_RELIC_API_KEY environment variable is set
+api_key = os.getenv('NEW_RELIC_API_KEY')
+if not api_key:
+    raise ValueError("No API key found. Set the NEW_RELIC_API_KEY environment variable.")
 
 def parse_lighthouse_report(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
-
-lighthouse_data = parse_lighthouse_report('report.json')
 
 def extract_metrics(lighthouse_data):
     # Extracting key metrics
@@ -22,30 +26,34 @@ def extract_metrics(lighthouse_data):
         "speedIndex": speed_index
     }
 
-new_relic_data = extract_metrics(lighthouse_data)
-
 def print_data_for_verification(data):
     print("Data to be sent to New Relic:")
     print(json.dumps(data, indent=4))
 
-print_data_for_verification(new_relic_data)
-
 def send_data_to_new_relic(data):
     logging.basicConfig(level=logging.DEBUG)
     headers = {
-        "Api-Key": "eu01xx527745a1e9849a14fa3a5df19bb731NRAL",
+        "Api-Key": api_key,
         "Content-Type": "application/json"
     }
-    url = "https://insights-collector.newrelic.com/v1/accounts/YOUR_ACCOUNT_ID/events"  # Replace YOUR_ACCOUNT_ID with your actual account ID
+    # Replace YOUR_ACCOUNT_ID with your actual account ID
+    url = "https://insights-collector.newrelic.com/v1/accounts/YOUR_ACCOUNT_ID/events"
 
     response = requests.post(url, headers=headers, json=[data])
 
     try:
         response.raise_for_status()
         print("Sent metrics successfully!")
+    except requests.HTTPError as e:
+        logging.error(f"HTTP error occurred: {e}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
 
     return response
 
-send_response = send_data_to_new_relic(new_relic_data)
+# Running the script
+if __name__ == "__main__":
+    lighthouse_data = parse_lighthouse_report('report.json')
+    new_relic_data = extract_metrics(lighthouse_data)
+    print_data_for_verification(new_relic_data)
+    send_response = send_data_to_new_relic(new_relic_data)
